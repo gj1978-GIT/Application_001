20f10d623b33b60ba8136d379b37cbc2da7a5f05
Gaurav_GIT <gaurav.jain1978@gmail.com>
2024-11-29 13:23:43 +0530

    Initial Commit

---
+++ Application_001/Application_001App.swift
@@ -0,0 +1,17 @@
+//
+//  Application_001App.swift
+//  Application_001
+//
+//  Created by Gaurav Jain on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Application_001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Application_001Tests/Application_001Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Application_001Tests.swift
+//  Application_001Tests
+//
+//  Created by Gaurav Jain on 29/11/24.
+//
+
+import Testing
+@testable import Application_001
+
+struct Application_001Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
